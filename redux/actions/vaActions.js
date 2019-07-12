abcc00a383f16fd102f97064defe333a5b6ea194
adhiwie <mail@adhiwie.web.id>
2019-07-12 16:18:56 +0100

    modify api call

--- redux/actions/vaActions.js
+++ redux/actions/vaActions.js
@@ -5,20 +5,21 @@ import sha1 from 'crypto-js/sha1';
 import {
   GENERATE_VA_TOKEN
 } from '../types';
-import { INTRAJASA_API } from '../../config';
+//import { INTRAJASA_API } from '../../config';
+import { API } from '../../config';
 
-const getToken = (merchantId, merchantRefCode, secretWord, custName, custEmail, totalAmount) => {
+const generateVA = (merchantId, merchantRefCode, secretWord, custName, custEmail, totalAmount) => {
   const secureCode = sha256(merchantId+merchantRefCode+sha1(secretWord).toString()).toString();
   return (dispatch) => {
-    axios.post(`${INTRAJASA_API}/gettoken`, {
+    axios.post(`${API}/generateVA`, {
       'merchantId': merchantId,
       'merchantRefCode': merchantRefCode,
-      'secureCode': secureCode
+      'secureCode': secureCode,
+      'custName': custName,
+      'custEmail': custEmail,
+      'totalAmount': totalAmount
     }).then((response) => {
-        console.log(response.data);
-        if (response.data.responseCode == "200") {
-          dispatch(generateVa(merchantId, merchantRefCode, secretWord, custName, custEmail, totalAmount, response.data.token));
-        };
+        console.log(response);
         //dispatch({type: USER_DATA, payload: response.data.user_data});
       })
       .catch((error) => {
@@ -28,39 +29,38 @@ const getToken = (merchantId, merchantRefCode, secretWord, custName, custEmail, 
   };
 };
 
-const generateVa = (merchantId, merchantRefCode, secretWord, custName, custEmail, totalAmount, token) => {
-  const secure = sha256(merchantId+token+merchantRefCode+custName+totalAmount).toString();
-  const data = {
-    'vaType': '2',
-    'merchantRefCode': merchantRefCode,
-    'merchantId': merchantId,
-    'customerData': {
-        'custName': custName,
-        'custAddress1': '',
-        'custAddress2': '',
-        'custAddress3': '',
-        'custPhoneNumber': '',
-        'custEmail': custEmail,
-        'custCountryCode': ''
-    },
-    'productData': [],
-    'totalAmount': totalAmount,
-    'secureCode': secure
-  };
-  console.log(data);
-  return (dispatch) => {
-    axios.post(`${INTRAJASA_API}/generateva?t=`+sha256(token).toString(), data).then((response) => {
-        console.log(response)
-        //dispatch({type: USER_DATA, payload: response.data.user_data});
-      })
-      .catch((error) => {
-        console.log(error);
-        throw new Error(error);
-      });
-  };
-}
+// const generateVa = (merchantId, merchantRefCode, secretWord, custName, custEmail, totalAmount, token) => {
+//   const secure = sha256(merchantId+token+merchantRefCode+custName+totalAmount).toString();
+//   const data = {
+//     'vaType': '2',
+//     'merchantRefCode': merchantRefCode,
+//     'merchantId': merchantId,
+//     'customerData': {
+//         'custName': custName,
+//         'custAddress1': '',
+//         'custAddress2': '',
+//         'custAddress3': '',
+//         'custPhoneNumber': '',
+//         'custEmail': custEmail,
+//         'custCountryCode': ''
+//     },
+//     'productData': [],
+//     'totalAmount': totalAmount,
+//     'secureCode': secure
+//   };
+//   console.log(data);
+//   return (dispatch) => {
+//     axios.post(`${API}/generateva?t=`+sha256(token).toString(), data).then((response) => {
+//         console.log(response)
+//         //dispatch({type: USER_DATA, payload: response.data.user_data});
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         throw new Error(error);
+//       });
+//   };
+// }
 
 export default {
-  getToken,
-  generateVa,
+  generateVA,
 };
