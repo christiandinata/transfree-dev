a014c2bece52238d52b83d161fbbcad3f691991e
fernndzaky <43334250+fernndzaky@users.noreply.github.com>
2019-08-28 12:06:01 +0700

    update block cache

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ const Header = () => (
   <div>
     <Head>
       <title>Transfree - International Transfer Made Fast and Free</title>
-      <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+      <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link rel="icon" href="../static/images/icon-logo.png"/>
