87651c1705f40b21bd4d819ae5cad2d3ba1f10d9
fernndzaky <43334250+fernndzaky@users.noreply.github.com>
2019-09-11 18:39:54 +0700

    add purpose of transfer

--- components/order/Recipient.js
+++ components/order/Recipient.js
@@ -238,14 +238,35 @@ class Recipient extends React.Component {
             onBlur={this.checkName}/>
           <span className={this.state.isNameValid ? 'error-label-hidden' : 'error-label'}>Recipient's full name may not be empty.</span>
 
-          <label htmlFor="email">( Optional ) Recipient's email address</label><br/>
+          <label htmlFor="email">( Optional )Purpose Of Transfer</label><br/>
+          <select
+              type="email"
+              id="email"
+              className="select-css"
+              ref={this.email}
+              defaultValue={this.props.data.email}>
+              <option value="Buying Overseas Property">Buying Overseas Property</option>
+              <option value="Paying for Overseas Tuition Fees">Paying for Overseas Tuition Fees</option>
+              <option value="Overseas Investments">Overseas Investments</option>
+              <option value="Sending Money to Friends or Family">Sending Money to Friends or Family</option>
+              <option value="Paying for Living Cost">Paying for Living Cost</option>
+              <option value="Supporting a Family Member Who is Travelling Overseas">Supporting a Family Member Who is Travelling Overseas</option>
+              <option value="Paying for Wedding Abroad">Paying for Wedding Abroad</option>
+              <option value="Overseas Mortgage Payments">Overseas Mortgage Payments</option>
+              <option value="Paying For Work to be Completed Overseas">Paying For Work to be Completed Overseas</option>
+              <option value="Paying For Bills Overseas">Paying For Bills Overseas</option>
+              <option value="others">Others</option>
+            </select>
+    {/*
           <input
             type="email"
             id="email"
             placeholder="name@example.com"
             ref={this.email}
             defaultValue={this.props.data.email}/>
-          <span className={this.state.isEmailValid ? 'error-label-hidden' : 'error-label'}>Email address is not valid.</span>
+            */}
+    
+          {/*<span className={this.state.isEmailValid ? 'error-label-hidden' : 'error-label'}>Email address is not valid.</span>*/}
           {
           // <input
           //   type="email"
@@ -354,6 +375,41 @@ class Recipient extends React.Component {
           </Link>
         </form>
         <style jsx>{`
+          /** DROP DOWN CSS **/
+          .select-css {
+            width: 100%;
+            margin-bottom: 30px;
+            border: none;
+            border-radius: 0;
+            font-size: 16px;
+            padding: 15px 0;
+            border-bottom: 1px solid #eaeaea;
+            font-family: "Campton-Book", sans-serif;
+            display: block;
+            box-sizing: border-box;
+            -moz-appearance: none;
+            -webkit-appearance: none;
+            appearance: none;
+            background-color: #fff;
+            background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E');
+            background-repeat: no-repeat, repeat;
+            background-position: right .7em top 50%, 0 0;
+            background-size: .65em auto, 100%;
+          }
+          .select-css::-ms-expand {
+            display: none;
+          }
+          .select-css:hover {
+            border-color: #469DDD;
+          }
+          .select-css:focus {
+            border-color: #469DDD;
+            outline: none;
+          }
+          .select-css option {
+            font-weight:normal;
+          }
+          /** END OF DROP DOWN CSS **/
           .div-show {
             display: block;
           }
