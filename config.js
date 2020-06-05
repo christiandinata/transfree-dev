e01056b7ca628812db779be2592049b22ac10904
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2020-06-05 17:34:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // API: 'https://api.transfree.id',
+    API: 'https://api.transfree.id',
     //API: 'http://transfree-api.uatech.co.id',
-    API: 'http://localhost:8000',
+    //API: 'http://localhost:8000',
     INTRAJASA_API: 'http://182.23.65.25:31555/vaonline/rest/json',
   };
