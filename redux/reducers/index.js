a0a39be487aaf8710fca075dadd1440bfbf9050d
Albert Simanjuntak <albertsimanjuntak02@gmail.com>
2020-06-19 12:54:56 +0700

    commit

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -11,6 +11,7 @@ import photoReducer from './photoReducer';
 import fxMarginReducer from './fxMarginReducer';
 import checkReducer from './checkReducer';
 import complaintReducer from './complaintReducer';
+import donasiReducer from './donasiReducer';
 const rootReducer = combineReducers({
   authentication: authReducer,
   verify: verifyReducer,
@@ -23,7 +24,8 @@ const rootReducer = combineReducers({
   photo: photoReducer,
   fx: fxMarginReducer,
   ck: checkReducer,
-  complaint : complaintReducer
+  complaint : complaintReducer,
+  qurban:donasiReducer,
 });
 
 export default rootReducer;
