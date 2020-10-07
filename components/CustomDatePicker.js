0d9de2dee6d0f74773ff428fbe95375fa41b0d07
Alvin Arrazy <51030862+alvinarrazy@users.noreply.github.com>
2020-10-07 20:52:33 +0700

    Update CustomDatePicker.js
    ditambahin komen di fungsi2nya

--- components/CustomDatePicker.js
+++ components/CustomDatePicker.js
@@ -9,37 +9,39 @@ const CustomDatePickerButton = ({ onClick }) => <div className="custom-date-pick
 
 function CustomDatePicker (props) {
   const date = moment(props.date ? props.date : new Date())
-
+  //Validasi tanggal
   function validateDate(inputDate) {
     if (inputDate < 1) {
       date.date(1)
       return
-    }
+    }//Jika tanggal yang dimasukkan kurang dari 1, kembalikan tanggal jadi 1
 
     if (inputDate > date.daysInMonth()) {
       date.date(date.daysInMonth())
       return
-    }
+    }/*Jika tanggal yang dimasukkan lebih besar dari maksimal tanggal, kembalikan tanggal jadi maksimal 
+    tanggal pada bulan tersebut*/
 
     date.date(inputDate)
   }
-
+  //Validasi Bulan
   function validateMonth(inputMonth) {
     if (inputMonth >= 1 && inputMonth <= 12) {
       date.month(inputMonth)
     }
-  }
+  }//Jika input tanggal dari 1 sampai 12, berarti input valid
 
+  //Validasi tahun
   function validateYear(inputYear) {
     if (inputYear < 0) {
       date.year(0)
       return
-    }
+    }//Tahun minimum 0
 
     if (inputYear > 99999) {
       date.year(99999)
       return
-    }
+    }//Tahun maksimum 99999
 
     date.year(inputYear)
   }
@@ -47,18 +49,18 @@ function CustomDatePicker (props) {
   function setDate(inputDate) {
     validateDate(inputDate)
     props.onChange(date.toDate())
-  }
+  }//Mengganti tanggal berdasarkan masukan
 
   function setMonth(inputMonth) {
     validateMonth(inputMonth)
     validateDate(date.date())
     props.onChange(date.toDate())
-  }
+  }//Mengganti bulan berdasarkan masukan
 
   function setYear(inputYear) {
     validateYear(inputYear)
     props.onChange(date.toDate())
-  }
+  }//Mengganti tahun berdasarkan masukan
 
   return(
     <div className='custom-date-picker'>
