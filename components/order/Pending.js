f224b970ed985225a5b34e5d101fb1fb6de8bfa5
Anna Elvira Hartoyo <naelvirahart@gmail.com>
2021-07-14 10:57:29 +0700

    add link for contacts on pending page

--- components/order/Pending.js
+++ components/order/Pending.js
@@ -21,6 +21,11 @@ const ContentContainer = styled.div`
   h2{
     font-size: 32px;
   }
+
+  span{
+    color: #009FE3;
+    cursor: pointer;
+  }
 `;
 
 const IconContainer = styled.div`
@@ -70,8 +75,8 @@ export function AwaitingConfirmation(){
         icon = {"../static/images/Asset Web/send money/ic-wait.svg"}
         title = {"Awaiting Account Confirmation"}
         desc = {<p>We are now reviewing your account details. We will send you an email & WhatsApp message once the verification process is completed.
-                <br/>Please contact us by email <span style={{color:'#009FE3'}}>admin@transfree.id</span> or 
-                WhatsApp <span style={{color:'#009FE3'}}>+44 7490 090659</span> for faster process.</p>}
+                <br/>Please contact us by email <span onClick={()=>window.open("mailto:admin@transfree.id")}>admin@transfree.id</span> or 
+                WhatsApp <span onClick={()=>window.open("https://api.whatsapp.com/send?phone=447490090659&text=Hello%20Transfree")}>+44 7490 090659</span> for faster process.</p>}
         location = {"/home"}
         buttonText = {"Back to Homepage"}
       />
